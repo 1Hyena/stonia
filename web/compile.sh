4072a4ec0be3e37ca37e67fa6b314d8c541704f0
1Hyena <hyena@hyena.net.ee>
2024-10-30 01:34:45 +0200

    Enhanced a kludgy hack.

--- web/compile.sh
+++ web/compile.sh
@@ -2,3 +2,5 @@
 ../../mdma/mdma --verbose -f framework.html --monolith --minify -o index.html \
     content.md
 sed -i 's/src=".\/files\/card.jpg"/src="files\/count.png"/' index.html
+sed -i 's/title="Illustration of a battle"/title="Player count chart"/' index.html
+sed -i 's/alt="Illustration of a battle"/alt="Player count chart"/' index.html
